6613c36ef0974132f6e34cc3974c8ab77168a5ed
KarthikSundar <karasundal2002@gmail.com>
2017-12-04 19:42:07 +0530

    task1 test1 take 3

--- zerver/lib/actions.py
+++ zerver/lib/actions.py
@@ -1640,7 +1640,7 @@ def check_send_message(sender, client, message_type_name, message_to,
         message_to,
         topic_name)
     if message_content=="welcome":
-	    message_content = "Welcome to Zulip :octopus:"
+        message_content = "Welcome to Zulip :octopus:"
 
     message = check_message(sender, client, addressee,
                             message_content, realm, forged, forged_timestamp,
